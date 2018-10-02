3b96f2319e46e6518755ffaeeae27b2b7a08c830
baconhsiao <43340459+baconhsiao@users.noreply.github.com>
2018-10-02 15:33:42 +0800

    Create sum.cpp

---
+++ w03/sum.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+	
+	int sum = 0;
+	int n1,n2;
+
+	printf ("Enter n1 n2 : ");
+	scanf ("%d",&n1);
+	scanf ("%d",&n2);
+	
+	for(int i = n1;i <= n2;++i){
+		sum += i;
+} 
+	printf ("sum(%d,%d) = %d. \n",n1,n2,sum);
+	
+	
+    while (n1>=0,n2>=0){
+    	
+ 	int sum = 0;
+	int n1,n2;
+	
+	printf ("Enter n1 n2 : ");
+	scanf ("%d",&n1);
+	scanf ("%d",&n2);
+	
+	for(int i = n1;i <= n2;++i){
+		sum += i;
+} 
+	printf ("sum(%d,%d) = %d. \n",n1,n2,sum);
+}
+}
